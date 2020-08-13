1346be848c757fe5fe49646036e2307cc37fe049
Alec Hill <web@ahill.io>
2020-08-13 15:07:13 -0400

    Watch.sh tweaks.

--- watch.sh
+++ watch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#while inotifywait -e modify -qq timewarrior-custom.py; do
+#  timew custom test
 while inotifywait -e modify -qq $1; do
-  #zsh $1
-  $1
+  # zsh $1
+  # something $1
+  # or just the script itself with an argument:
+  $1 $2
 done
