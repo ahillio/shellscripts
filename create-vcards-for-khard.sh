46432efc8f925902d67855335f7c80919a653f22
Alec Hill <web@ahill.io>
2020-09-25 17:43:27 -0400

    rename khard vcard creator.

--- create-vcards-for-khard.sh
+++ create-vcards-for-khard.sh
@@ -14,5 +14,6 @@ for FILE in $(ls); do mv $FILE $(random.sh 36).vcf; done
 for FILE in *; do
   RND=$(echo $FILE | rev | cut -c5- | rev)
   STRING="UID:$RND"
+  # @Syntax insert text at line#3, \\ to escape $var
   sed "3i\\$STRING" $FILE > ../$FILE #note @TODO update file path, here we're running the command on a subdirectory of the ~/.contacts dir and we're copying the updated files to the parent directory
 done
