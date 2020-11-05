5fa3cc6f7d7e1d08d36640713553f5dd8d0a69e3
Alec Hill <web@ahill.io>
2020-11-05 12:45:29 -0500

    Add stripe-invoices.sh

---
+++ stripe-invoices.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+function stripe-invoices {
+  APIKEY=$(cat ~/.passwords/stripe-live-sk)
+  curl https://api.stripe.com/v1/invoices -u $APIKEY: -G |
+    #works, unsorted
+    #jq -S -C '.data[] | {invoice_id: .id, client: .customer_name, date: .date | strftime("%Y-%m-%d"), amount: .total, status: .status} | .amount = "$" + (.amount/100|tostring)'
+    # to sort this took a lot of time, formerly unsorted, the following is sorted:
+    jq -S -C '[.data[] | {invoice_id: .id, client: .customer_name, date: .date | strftime("%Y-%m-%d"), amount: .total, status: .status} | .amount = "$" + (.amount/100|tostring)] | sort_by(.date)'
+    # https://unix.stackexchange.com/questions/613779/how-to-get-the-right-type-and-value-for-jq-to-sort
+  }
+
+#stripe-invoices
+cat inc/stripe.json | jq -S -C '[.data[] | {invoice_id: .id, date: .date | strftime("%Y-%m-%d"), amount: .total,} | .amount = "$" + (.amount/100|tostring)] | sort_by(.date)'
