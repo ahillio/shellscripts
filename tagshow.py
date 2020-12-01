0f2b5cb6666e33f2da49f527da33e2d908db3c27
Alec Hill <web@ahill.io>
2020-12-01 10:48:02 -0500

    Tag report: consolidate endoftag regex.

--- tagshow.py
+++ tagshow.py
@@ -29,20 +29,8 @@ for filename in sorted(os.listdir(directory)):
                 tagsection = section
                 copy = True
                 continue
-            # stop copying when reaching a new section header
-            elif re.search(r'^##', line):
-                copy = False
-                continue
-            # stop copying when reaching a line break
-            elif re.search(r'^---', line):
-                copy = False
-                continue
-            # stop copying when reaching a blank line
-            elif re.search(r'^\s*$', line):
-                copy = False
-                continue
-            # stop copying when reaching a new tag
-            elif re.search(r'^:\w*:', line):
+            # stop copying when tag content is complete
+            elif re.search('^##|^---|^\s*$|^:\w*:|^- TODO', line):
                 copy = False
                 continue
             # now add lines that follow tagname to the tagcontent variable
