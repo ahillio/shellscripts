cf82178e74eb5cb87ba1f67484f979c8c4428407
Alec Hill <web@ahill.io>
2021-01-25 19:19:59 -0500

    Update tex generation script.

--- gentex.sh
+++ gentex.sh
@@ -4,20 +4,25 @@
 
 [ "$#" = 1 ] || { echo Supply filetype as argument; return 9; }
 codefiles=$(ls | grep ".$1" | sed "s|\.$1||g")
+rm *tex
 for file in $codefiles; do
   if [ -e $file.tex ]
   then
     echo "$file.tex exists already.  Now creating pdf file..."
-    pdflatex --shell-escape $file.tex
+    latex --shell-escape $file.tex
+    #pdflatex --shell-escape $file.tex
+    #cat $file.tex | xargs -0 -t -I % /usr/local/lib/node_modules/mathjax-node-cli/bin/tex2svg '%' > $file.svg
   else
     cp ~/bin/inc/template.tex $file.tex
     sed -i "s/filetype/$1/" $file.tex
     sed -i "s/example/$file.$1/" $file.tex
+    #latex --shell-escape $file.tex
     pdflatex --shell-escape $file.tex
+    #cat $file.tex | xargs -0 -t -I % /usr/local/lib/node_modules/mathjax-node-cli/bin/tex2svg '%' > $file.svg
   fi
-  pdfcrop $file.pdf
-  rm $file.pdf
-  mv $file-crop.pdf $file.pdf
+  #rm $file.pdf
+  #mv $file-crop.pdf $file.pdf
   rm $file.aux $file.log texput.log
   rm -rf _minted-$file
+  #mv $file.dvi $file.pdf
 done
