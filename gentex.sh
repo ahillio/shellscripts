bc405213930b33cc434fa20411802c740a03a68d
Alec Hill <web@ahill.io>
2021-03-11 16:25:36 -0500

    Tex stuff.

--- gentex.sh
+++ gentex.sh
@@ -7,10 +7,12 @@ codefiles=$(ls | grep ".$1" | sed "s|\.$1||g")
 for file in $codefiles; do
   cp ~/bin/inc/template.tex $file.tex
   linelength=$(wc -L $file.$1 | grep -Po "\\d+")
-  pagewidth=$(echo "$linelength * .33" | bc)
+  pagewidth=$(echo "$linelength * .34" | bc)
   sed -i "s/pagewidth/$pagewidth/" $file.tex
   sed -i "s/filetype/$1/" $file.tex
   sed -i "s/example/$file.$1/" $file.tex
+  # if $1=='php' then set [firstline=2,firstnumber=1]
   pdflatex --shell-escape $file.tex
 done
 rm -rf _minted* *.tex *.aux *.log
+# pdfcrop --margins '5 -18 5 5' --clip create_array.pdf create_array.pdf
